8b3c4ee7c981a915340109bf997977c78df5a8a3
Aelzyat0 <122340035+Aelzyat0@users.noreply.github.com>
2025-06-13 07:00:53 +0300

    resumePageData.js

--- constants/resumePageData.js
+++ constants/resumePageData.js
@@ -34,7 +34,7 @@ import {
 const about = {
   title: "About Me",
   description:
-    "Ahmed Elzyat | MIS graduate | Web developer, modern technology solutions and customized AI tools for tasks _💡 I have been working in the field of web and systems development for more than 3 years, I am passionate about providing innovative solutions that help solve issues and save time and effort using technology _ 💻",
+    "Founder @ Evolentra | Solving Tech Challenges | Crafting Custom Systems Solutions_ 💻",
   info: [
     {
       fieldName: "Name",
@@ -65,14 +65,6 @@ const about = {
       fieldValue: "Available",
     },
     {
-      fieldName: "part-time",
-      fieldValue: "Available",
-    },
-    {
-      fieldName: "Remote",
-      fieldValue: "Available",
-    },
-    {
       fieldName: "Languages",
       fieldValue: "English, Arabic",
     },
@@ -85,14 +77,14 @@ const experience = {
     "I have more than three years of experience in technical solutions and web development, I have used different technologies and frameworks to create complete, robust and user-friendly applications that meet the needs of clients, and I have a strong foundation in front-end and back-end development by working in different sectors that help in a good understanding of the business.",
   items: [
     {
-      position: "Technical Solutions Engineer",
-      company: "Connect Wave Agency",
-      date: " May 2025 - Present ·",
+      position: "Software Engineer",
+      company: "Self Employed",
+      date: " Feb 2025 - Present",
     },
     {
-      position: "Broadcast Technology",
+      position: "Broadcast Technology Specialist",
       company: "The Egyptian Media Production City ·",
-      date: " Feb 2025 - Apr 2025 ·",
+      date: " Feb 2025 - Apr 2025",
     },
     {
       position: "Software Developer",
@@ -101,7 +93,7 @@ const experience = {
     },
     {
       position: "Information Technology",
-      company: "B-Tech Solutions",
+      company: "B-Connect Solutions",
       date: "Jan 2023 - Jan 2024 ·",
     },
     {
@@ -118,39 +110,39 @@ const education = {
     "I am a university student with a Bachelor's degree (with a unique graduation project) in Information Systems from the Higher Institute for Qualitative Studies. I have completed many training courses and certifications in programming, computer science, project management, IT security, learning how to learn, issue resolution skills, and flexibility to work with the latest tools, which enhanced my skills and knowledge in these fields.",
   items: [
     {
-      degree: "BSc. (Hons) in Computing & Information Systems",
-      university: "SUSL, Belihuloya",
-      date: "2022 - Present",
+      degree: "Bachelor`s degree Management Information Systems,",
+      university: "Higher Institute for Qualitative Studies",
+      date: "jan 2018 - May 2022",
     },
     {
       degree: "Professional Skill Program on Agility",
       university: "SLASSCOM, Colombo",
-      date: "20223 Oct",
+      date: "2023 - Oct",
     },
     {
       degree: "Agile Project Management",
       university: "Coursera",
-      date: "2023 Sep",
+      date: "2023 - Sep",
     },
     {
       degree: "Foundations of Digital Marketing & E-commerce",
       university: "Coursera",
-      date: "2023 Aug",
+      date: "2023 - Aug",
     },
     {
       degree: "IT Security, Defence Against the Digital Dark",
       university: "Coursera",
-      date: "2023 May",
+      date: "2023 - May",
     },
     {
       degree: "The Fundamentals of Digital Marketing",
       university: "Google Digital Garage",
-      date: "2023 Apr",
+      date: "2023 - Apr",
     },
     {
       degree: "Diploma in Information Technology (DIT)",
-      university: "ICBT Campus, Colombo",
-      date: "2021 Apr - 2021 Oct",
+      university: "ICBT Campus",
+      date: "2021 - Apr - 2021 Oct",
     },
   ],
 };
@@ -158,7 +150,7 @@ const education = {
 const skills = {
   title: "My Skills",
   description:
-    "I have experience working with different technologies and frameworks, such as React, Node.js, next.js, and Vibe coding, which enables me to build robust and scalable applications, and I am familiar with Agile methodologies, which helps me work effectively in team environments and deliver projects on time.",
+    "I have experience working with different technologies and frameworks, such as React, Node.js, next.js, which enables me to build robust and scalable applications, and I am familiar with Agile methodologies, which helps me work effectively in team environments and deliver projects on time.",
   skillList: [
     {
       icon: <SiHtml5 />,
